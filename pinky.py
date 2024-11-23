30bb684f7e4a6210604e0b3b35a9c239f6ca4374
marabend <dev.marcel.abend@gmail.com>
2024-11-23 11:23:17 +0100

    Handling of single character tokens

--- pinky.py
+++ pinky.py
@@ -1,13 +1,16 @@
-
 import sys
+from tokens import *
+from lexer import *
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        raise SystemExit('Usage: python3 pinky.py <filename>')
-    filename = sys.argv[1]
-    print(filename)
-    with open(filename) as file:
-        source = file.read()
-        print(source)
+  if len(sys.argv) != 2:
+    raise SystemExit('Usage: python3 pinky.py <filename>')
+  filename = sys.argv[1]
+  print(filename)
 
-        # TODO: tokenize the input source
+  with open(filename) as file:
+    source = file.read()
+
+    print("LEXER:")
+    tokens = Lexer(source).tokenize()
+    for tok in tokens: print(tok)
